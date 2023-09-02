887cb22f4b69436244e0449d47c59593e23f882f
JonasPrade <j.prade@campus.tu-berlin.de>
2023-09-02 13:11:32 +0200

    Footer.js bigger top margin

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,7 +2,7 @@ import {Container} from "react-bootstrap";
 
 function Footer() {
     return(
-            <Container className="mt-3 pb-1 text-muted fixed-bottom bg-background" >
+            <Container className="mt-2 pb-1 text-muted fixed-bottom bg-background" >
                 <a href="https://jonas-prade.de/" className="text-muted">Impressum und Kontakt</a>
                 <span> | </span>
                 <a target="_blank" href="https://github.com/JonasPrade/schienendashboard_reporting" rel="noreferrer" className="text-muted">Fehler melden</a>
