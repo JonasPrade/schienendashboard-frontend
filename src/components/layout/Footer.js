fd20153ef75f27e11bb1e9fce978852798dc5213
JonasPrade <j.prade@campus.tu-berlin.de>
2023-09-16 12:03:19 +0200

    no cookie information in Footer.js

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,10 +2,12 @@ import {Container} from "react-bootstrap";
 
 function Footer() {
     return(
-            <Container className="mt-2 pb-1 text-muted fixed-bottom bg-background" >
+            <Container className="mt-4 pb-1 text-muted fixed-bottom bg-background" >
                 <a href="https://jonas-prade.de/" className="text-muted">Impressum und Kontakt</a>
                 <span> | </span>
                 <a target="_blank" href="https://github.com/JonasPrade/schienendashboard_reporting" rel="noreferrer" className="text-muted">Fehler melden</a>
+                <span> | </span>
+                <span>Es werden keine Cookies genutzt</span>
             </Container>
     )
 }
