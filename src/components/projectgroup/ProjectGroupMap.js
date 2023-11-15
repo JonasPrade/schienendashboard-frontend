e21b36bc2a8abcee53b8315f2674e3d69c703694
JonasPrade <j.prade@campus.tu-berlin.de>
2023-11-15 17:44:44 +0100

    plot all Projects possible
    option in backend was added that says if ProjectGroup should plot all or only the superior projects.
    This is now implemented in Frontend

--- src/components/projectgroup/ProjectGroupMap.js
+++ src/components/projectgroup/ProjectGroupMap.js
@@ -7,9 +7,27 @@ function ProjectGroupMap(props) {
     const allProjectContentsWithColor = props.projectGroups.flatMap(group =>
         group.projects_content.map(projectcontent => ({
             ...projectcontent,
-            color: group.color
+            color: group.color,
+            plot: group.plot_only_superior_projects === false ? true : false
         }))
     );
+
+    const allProjectContents = []
+
+    for (let Group of props.projectGroups) {
+        for (let Project of Group.projects_content) {
+            Project["color"] = Group.color;
+            if (Group.plot_only_superior_projects === false) {
+                Project["plot"] = true;
+            } else {
+                if (Project.superior_project_content_id === null) {
+                    Project["plot"] = true;
+                }
+            }
+            allProjectContents.push(Project);
+        }
+    }
+
 
     // it will plot all Projects that have NO superior project
     return(
@@ -19,8 +37,8 @@ function ProjectGroupMap(props) {
                     attribution='Kartenhintergrund: <a href="https://www.bkg.bund.de">Bundesamt für Kartographie und Geodäsie</a>'
                     url={process.env.REACT_APP_TILE_LAYER_URL}
                 />
-                {allProjectContentsWithColor.map(projectcontent => (
-                    !projectcontent.superior_project_content_id &&
+                {allProjectContents.map(projectcontent => (
+                    projectcontent.plot &&
                         <ProjectGeoJson
                             key={projectcontent.id}
                             projectcontent={projectcontent}
