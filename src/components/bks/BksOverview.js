67ec5d5fa46e065b5f137de38fd416a4c978ac79
JonasPrade <j.prade@campus.tu-berlin.de>
2023-12-18 16:04:41 +0100

    removed the counts from BksOverview

--- src/components/bks/BksOverview.js
+++ src/components/bks/BksOverview.js
@@ -4,42 +4,10 @@ import BksProgress from "./BksProgress";
 
 function BksOverview(props){
 
-    function get_counts(bks_data) {
-        var count_handlungsfelder = 0;
-        var count_clusters = 0;
-        var count_actions = 0;
-        if (bks_data != null) {
-            count_handlungsfelder = bks_data.length
-            for (var handlungsfeld of bks_data) {
-                count_clusters += handlungsfeld.cluster.length
-                for (var cluster of handlungsfeld.cluster) {
-                    count_actions += cluster.bks_action.length
-                }
-            }
-        }
-        return [count_handlungsfelder, count_clusters, count_actions]
-    }
-
-
-
-    const [count_handlungsfelder, count_clusters, count_actions] = get_counts(props.data)
-
-
-
     return(
         <div className="bg-light p-3 mt-3 border border-primary rounded">
             <h2>Übersicht</h2>
             <Row>
-                <Col xl={6} m={12}>
-                    <div>
-                        <ul>
-                            <li>{count_handlungsfelder} Handlungsfelder</li>
-                            <li>{count_clusters} Cluster</li>
-                            <li>{count_actions} Maßnahmen</li>
-                        </ul>
-                    </div>
-
-                </Col>
                 <Col xl={6} m={12}>
                     <div>
                         <h5>Dokumente</h5>
