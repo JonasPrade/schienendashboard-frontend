55ae532bd8c468dec1d995bd64bcad3828106515
JonasPrade <j.prade@campus.tu-berlin.de>
2024-03-01 11:25:47 +0100

    ProjectGroup Sidebar new order of objects
    selected Project is now on top.

--- src/components/projectgroup/ProjectGroupMapSidebar.js
+++ src/components/projectgroup/ProjectGroupMapSidebar.js
@@ -2,25 +2,35 @@ import {Spinner} from "react-bootstrap";
 import ProjectsSearchByString from "../project/ProjectDetail/ProjectsSearchByString";
 import ProjectGroupSelection from "./ProjectGroupSelection";
 import ProjectItemShort from "../project/ProjectItemShort";
-import {useState} from "react";
+import React, {useState} from "react";
 
 function ProjectGroupMapSidebar(props) {
     return(
         <div>
+            <div className="mt-3">
+                {props.selectedProject &&
+                    <div>
+                        <h3>Ausgewähltes Projekt:</h3>
+                        <ProjectItemShort project={props.selectedProject}/>
+                    </div>
+                }
+            </div>
             {props.isLoading ? (
                 <div className="d-flex justify-content-center mt-5">
                     <Spinner animation="border" role="status" variant="primary">
                     </Spinner>
                 </div>
-            ):(
-                <ProjectsSearchByString
-                    selectedGroupIds={props.selectedGroupIds}
-                    projects={props.projects}
-                    setProjects={props.setProjects}
-                    setIsLoading={props.setIsLoading}
-                    showSubprojects={props.showSubprojects}
-                    setShowSubprojects={props.setShowSubprojects}
-                />
+            ) : (
+                <div className="mt-3">
+                    <ProjectsSearchByString
+                        selectedGroupIds={props.selectedGroupIds}
+                        projects={props.projects}
+                        setProjects={props.setProjects}
+                        setIsLoading={props.setIsLoading}
+                        showSubprojects={props.showSubprojects}
+                        setShowSubprojects={props.setShowSubprojects}
+                    />
+                </div>
             )}
             <div className="mt-3">
                 <ProjectGroupSelection
@@ -28,9 +38,7 @@ function ProjectGroupMapSidebar(props) {
                     setSelectedGroupIds={props.setSelectedGroupIds}
                 />
             </div>
-            <div className="mt-3">
-                {props.selectedProject && <ProjectItemShort project={props.selectedProject}/>}
-            </div>
+
         </div>
     )
 }
